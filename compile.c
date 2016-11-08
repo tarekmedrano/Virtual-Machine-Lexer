adc5e750ecddf82ce35e387a338b32ee032a495b
tarekmedrano <tarekmedrano@knights.ucf.edu>
2016-11-08 16:25:23 -0500

    Update compile.c

--- compile.c
+++ compile.c
@@ -7,11 +7,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define MAX_CODE_LENGTH 500
 #define MAX_SYMBOL_TABLE_SIZE 10000
 
+
 typedef struct{
-  char type[3]
+  char type[3];
   char val[12];
   } token;
   
@@ -27,33 +29,30 @@ typedef struct{
   int val;			// number (ascii val)
   int level;		// L level 
   int addr;			// M address
-  } symbol_t;
+  } symbol;
 
-symbol_t symbol_table[MAX_SYMBOL_TABLE_SIZE];
+// All our global Vars
+
+symbol symbol_table[MAX_SYMBOL_TABLE_SIZE];
+int symIndex=0;
+int symAddr = 3;
 
 
 // Code array
-code_struct code[MAX_CODE_LENGTH] 
-
-// Code index
+code_struct code[MAX_CODE_LENGTH] ;
 int cx = 0;
+int curr_lvl = 0;
 
-//Stack pointer
-int sp;
-
-//Max lexicographical level
-int lvl;
-
-//Current Lexicographical level
-int curr_lvl;
-
-// Current lex level
-token current_token;
+int tokenArray[MAX_CODE_LENGTH];
+int current_token;
+int tokenIndex =0; int tokenPtr =0;
 
 // keep track of symbols in symbol table?
 int symbol_cnt;
 
-FILE *Parserin;
+FILE* Parserin;
+
+// Function prototypes
 
 int get_next_t();
 
@@ -62,10 +61,9 @@ void add_symbol(int k, char name[], int val, int addr);
 void err(int n);
 
 //Generates the PL/0 code line w/ the input fields.
-void emit(int op, int r, int l, int m);
+void emit(int op, int l, int m);
 
 //procs for the grammar
-
 void program();
 void statement();
 void block();
@@ -99,23 +97,13 @@ void condition(){
 
 int rel_op(){
 	
+	
+	
 }
 
-// Tarek
+
 void term(){
-	int mulop;
-	factor();
-	while(token == multsym || token == slashsym){
-		mulop = token;
-		get_next_t();
-		factor();
-		if(mulop == multsym)
-			emit(OPR, 0, OPR_MUL); //multiplication
-		else
-			emit(OPR, 0, OPR_DIV); //division
-		
-		
-	}
+
 	
 }
 
@@ -125,31 +113,12 @@ void factor(){
 }
 
 
-// Tarek
+
 void expression(){
-	int addop;
-	if (token == plussym || token == minussym){
-		addop = token;
-		get_next_t();
-		term();
-		if(addop == minussym) emit(OPR, 0, OPR_NEG); // negate
-	}
-	else term();
-	
-	while (token == plussym || token == minussym){
-		addop = token;
-		get_next_t();
-		term();
-		if(addop == plussym)
-			emit(OPR, 0, OPR_ADD); //addition
-		else
-			emit(OPR, 0, OPR_SUB); // subtraction
-		
-		
+
 		
 	}
 	
-}
 
 int get_next_t(){
 	
@@ -161,9 +130,11 @@ void err(int n){
 }
 
 // Tarek
-void emit(int op, int r, int l, int m){
-	if(cx > CODE_SIZE)
-		error(25);
+void emit(int op, int l, int m){
+	
+	// send an error saying too large
+	if(cx > MAX_CODE_LENGTH)
+		err(25);
 	else
 	{
 		code[cx].op = op; 	// opcode
