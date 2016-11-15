3c975b891ef8d9bda893bcbde882d154b3cde62b
Austin Dragone <austindrag1@gmail.com>
2016-11-15 16:33:57 -0500

    Update compile.c (see inside for edits)
    I edited a lot of the functions to reduce space and so it looks more like the appendix in http://www.cs.ucf.edu/~wocjan/Teaching/2016_Fall/cop3402/2_homeworks/IntermediateCodeGeneration.pdf

--- compile.c
+++ compile.c
@@ -25,13 +25,13 @@ intsym = 29, procsym = 30, outsym = 31, insym = 32, elsesym = 33};
 typedef struct{
   char type[3];
   char val[12];
-  } token;
+} token;
   
 typedef struct{
   int op;
   int l;
   int m;
-  }code_struct;
+}code_struct;
   
 typedef struct{
   int kind;			// const = 1, var = 2, proc = 3
@@ -39,7 +39,7 @@ typedef struct{
   int val;			// number (ascii val)
   int level;		// L level 
   int addr;			// M address
-  } symbol;
+} symbol;
 
 // All our global Vars
 
@@ -59,11 +59,7 @@ int current_token;
 int tokenIndex = 0;
 int tokenPtr = 0;
 
-
-
 FILE* Parserin;
-
-// Function prototypes
 
 void get_next_t(); // turned this to a void, was int
 void add_symbol(int k, char name[], int val, int addr);
@@ -77,7 +73,7 @@ void program();
 void statement();
 void block();
 void condition();
-int rel_op();
+bool rel_op();
 void term();
 void factor();
 void expression();
@@ -102,10 +98,10 @@ void program(){
 void block(){
 	
 	// Constant declaration
-	// EBSF: const id equals num { comma id = num } semicolon
+	// EBSF: "const" <id> "=" <num> { "," <id> "=" <num> } ";"
 	if( current_token == constsym ) {
 		
-		// Loop until no more constants declared (when we see a comma)
+		// Loop until no more constants declared (so until semicolon)
 		// Must see identifier, equals sign, then a number
 		while( current_token == commasym ) {
 			
@@ -116,7 +112,7 @@ void block(){
 			if( current_token != eqlsym ) ; // raise error
 			get_next_t();
 			
-			if( current_token != identsym ) ; // raise error
+			if( current_token != numbersym ) ; // raise error
 			get_next_t();
 		}
 		
@@ -125,10 +121,9 @@ void block(){
 	}
 	
 	// Variable declaration
-	// EBSF: var id { comma id } semicolon
+	// EBSF: "var" <id> { "," <id> } ";"
 	if( current_token == varsym ) {
 		
-		// 
 		while( current_token == commasym ) {
 			get_next_t();
 			if( current_token != identsym ) ; // raise error
@@ -140,7 +135,7 @@ void block(){
 	}
 	
 	// Procedure (Note: unused in tiny PL/0)
-	// EBSF: proc { identifier semicolon block semicolon }
+	// EBSF: "proc" { <id> ";" <block> ";" }
 	while( current_token == procsym ) {
 		
 		get_next_t();
@@ -158,57 +153,62 @@ void block(){
 	// Call statement
 	statement();
 }
+
 // jerasimos
 void statement(){
+	
 	if(current_token == identsym){
 		get_next_t();
-		// what sym is :=
+		if( current_token != becomessym ) ; // raise error
+		get_next_t();
 		expression();
 		return;
-	}else if(current_token == beginsym){
+		
+	} else if( current_token == callsym ) {
+		get_next_t();
+		if( current_token != identsym ) ; // raise error
+		get_next_t();
+		return;
+		
+	} else if(current_token == beginsym){
 		get_next_t();
 		statement();
-		while(current_token == semicolonsym)
-			statement();
-		if(current_token == endsym){
+		while(current_token == semicolonsym) {
 			get_next_t();
-			return;
-		}else{
-			printf("error encountered");
-			//error. 
+			statement();
 		}
-	}else if(current_token == ifsym ){
+		if(current_token != endsym) ; // raise error
+		get_next_t();
+		return;
+		
+	} else if(current_token == ifsym ){
 		get_next_t();
 		condition();
-		if(current_token == thensym){
-			get_next_t();
-			statement()
-		}else{
-			printf("error encountered");
-			//error.
-		}
-
-	}else if(current_token == readsym ){
+		if(current_token != thensym) ; // raise error
 		get_next_t();
-		if(	current_token == oddsym){
-			get_next_t();
-			return;
-		}else{
-			printf("error encountered");
-			//error.
-		}
-	}else if(current_token == writesym){
+		statement()
+		return;
+		
+	} else if(current_token == whilesym) {
 		get_next_t();
-		if(	current_token == oddsym){
-			get_next_t();
-			return;
-		}else{
-			printf("error encountered");
-			//error.
-		}
-
-
-	}else
+		condition();
+		if( current_token != dosym ) ; // raise error
+		get_next_t();
+		statement();
+		return;
+		
+	} else if(current_token == readsym ){
+		get_next_t();
+		if(	current_token != oddsym) ; // raise error
+		get_next_t();
+		return;
+		
+	} else if(current_token == writesym){
+		get_next_t();
+		if(	current_token != oddsym) ; // raise error
+		get_next_t();
+		return;
+	} 
 }
 
 // jerasimos 
@@ -219,7 +219,8 @@ void condition(){
 		return;
 	}else{
 		expression();
-		rel_op();
+		if( !rel_op() ) ; // error raised in rel_op()
+		get_next_t();
 		expression();
 		return;
 	}
@@ -227,47 +228,41 @@ void condition(){
 }
 
 // jerasimos
-int rel_op(){
+// Check that current_token is a relation symbol. If not, raise an error.
+bool rel_op(){
 	
-	// current_token should be the current token value and not an int
 	switch(current_token){
 
 	case eqlsym : // '='
-
-		break;
+		return true;
 	
-	case neqsym : //'<>': hope neqsym is <>
-
-		break;
+	case neqsym : //'<>' "This is correct" - Austin
+		return true;
 	
 	case lessym : // '<'
-
-		break;
+		return true;
 	
 	case leqsym : // '<='
-
-		break;
+		return true;
 
 	case gtrsym : // '>'
-
-		break;
+		return true;
 	
 	case geqsym : // '>='
-
-		break;
+		return true;
 
 	default :
 		printf("error sysntax error, need Relational Operators");
 		//error sysntax error, need Relational Operators
 		// error msg and exit() code need here!!
 	}
-	get_next_t();
 }
 
 // jerasimos
 void term(){
 	factor();
 	while(current_token == multsym || current_token == slashsym){
+		get_next_t();
 		factor();
 	}
 	
@@ -298,102 +293,102 @@ void factor(){
 		get_next_t();
 	}
 	
-	
 	else if(current_token == numbersym){
 		get_next_t();
 		emit(lit, 0, current_token);
-		token = get_next_t();
+		get_next_t();
 	}
 	
 	else if(current_token == lparentsym){
 		get_next_t();
-		current_token = expression(); // possible edit after experssion() finished
-		if(current_token != rparentsym){
-			
-			// fill this in with an error?
-		}
+		expression();
+		if(current_token != rparentsym) ; // raise error
 		
 		get_next_t();
-		
 	}
 	
-	else{
-		
-		//print out an invalid factor error
-	}
+	else ; // raise error
 }
 
 
 // jerasimos
 void expression(){
-	if(current_token == plussym || current_token == minussym){
+	if(current_token == plussym || current_token == minussym)
 		get_next_t();
-	}
-	if(current_token == identsym ){
-	}else{
-		//error missing +,-, or <indent>
-		printf("error missing +,-, or <indent>");
-	}
+	term();
 	while(current_token == plussym || current_token == minussym){
+		get_next_t();
 		term();
 	}
 }
-	
-// "Current_token was already a global so I changed made a few adjustments
-// to this and to factor()" - Austin
-// 
+
 // Gets the next token in the tokenArray and advances the pointer
 void get_next_t() {
 	current_token = tokenArray[tokenPtr];
 	tokenPtr ++;
 }
 
-// Fill this function with all the different errors we can get
-// use switches preferably?
-//I'm filling in the errors that I see are possible, will need editing for usability after. - Gabriela
+// Print an error statement out and exit the program.
+// "Page 5 of the following link should include all of the errors we need" - Austin
+// http://www.cs.ucf.edu/~wocjan/Teaching/2016_Fall/cop3402/2_homeworks/IntermediateCodeGeneration.pdf
+// I'm filling in the errors that I see are possible, will need editing for usability after. - Gabriela
 void err(int n){
 	switch(n) {
+		
 		case 1:
 			printf("Error: File must end in a period.");
 			break;
+			
 		case 2:
 			printf("Error: Missing identifier.");
 			break;
+			
 		case 3: 
 			printf("Error: Identifier should be followed by =");
 			break;
+			
 		case 4:
 			printf("Error: = should be followed by a number.");
 			break;
-	        case 5:
+			
+	    case 5:
 			printf("Error: Declaration must end with ;");
 			break;
+			
 		case 6:
 			printf("Error: Missing procedure declaration.");
 			break;
-	        case 7:
+			
+	    case 7:
 			printf("Error: No ; at the end of block.");
 			break;
-	        case 8:
+			
+	    case 8:
 			printf("Error: Missing := in statement.");
 			break;
+			
 		case 9:
 			printf("Error: Begin must be closed with end.");
 			break;
-	        case 10:
+			
+	    case 10:
 			printf("Error: if must be followed by then.");
 			break;
+			
 		case 11:
 			printf("Error: while must be followed by do.");
 			break;
+			
 		case 12:
 			printf("Error: Relational operator missing in conditional statement.");
 			break;
-	        case 13:
-	                printf("Error: Left ( has not been closed.");
-		        break;
+			
+	    case 13:
+	        printf("Error: Left ( has not been closed.");
+		    break;
+			
 		case 14:
-		        printf("Error: Identifier, (, or number expected.");
+		    printf("Error: Identifier, (, or number expected.");
 	}
 }
 
